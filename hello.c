3b6edb136a5771824d9884d216337e3877eae822
robo-monk <rrobomonk@gmail.com>
2022-11-25 11:58:00 +0000

    fib

--- hello.c
+++ hello.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+
+int fib(int n) {
+  if (n <= 1) {
+    return 1;
+  }
+
+  return fib(n-1) + fib(n-2);
+}
+
+
 int main(void) {
   printf("Hello mother fuckers what's up");
+  const int i = 42;
+  const int a = fib(i);
+  
+  printf("fib(%u) -> %u", i, a);
 }
+
 
